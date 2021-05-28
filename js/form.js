b3cabee2697512e742f62d932253842976639f96
parnikadalan <parnika17dalan@gmail.com>
2021-05-28 08:34:04 +0530

    fruit

--- js/form.js
+++ js/form.js
@@ -1,10 +1,10 @@
-class Form{
-    constructor(){
-       this.input = createInput("Name");
-       this.button = createButton('Play');
-       this.greeting = createElement('h2');
-       this.title = createElement('h2');
-       this.reset = createButton('Reset');
+class Form {
+    constructor() {
+        this.input = createInput("Name");
+        this.button = createButton('Play');
+        this.greeting = createElement('h2');
+        this.title = createElement('h2');
+        this.reset = createButton('Reset');
     }
     hide() {
         this.greeting.hide();
@@ -14,14 +14,14 @@ class Form{
     }
     display() {
         this.title.html("FRUIT CATCHER");
-        this.title.position(350, 50);
+        this.title.position(650, 50);
         this.title.style('font-size', '70px');
         this.title.style('color', 'skyblue');
-        this.input.position(550,400);
+        this.input.position(550, 400);
         this.input.style('width', '200px');
         this.input.style('height', '20px');
         this.input.style('background', 'lavender');
-        this.button.position(560,500);
+        this.button.position(560, 500);
         this.button.style('width', '200px');
         this.button.style('height', '40px');
         this.button.style('background', 'lightpink');
@@ -39,7 +39,7 @@ class Form{
             player.update();
             player.updateCount(playerCount);
             this.greeting.html("Hello " + player.name)
-            this.greeting.position(400,250);
+            this.greeting.position(550, 250);
             this.greeting.style('color', 'white');
             this.greeting.style('font-size', '100px');
         });
